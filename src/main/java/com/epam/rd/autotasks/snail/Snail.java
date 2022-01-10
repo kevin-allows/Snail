79440155d5345d07b4ac13d75c2de0774e254fe6
Ilya Mankouski <ilya.mankouski@gmail.com>
2022-01-10 13:46:12 +0100

    Snail - Try1

--- src/main/java/com/epam/rd/autotasks/snail/Snail.java
+++ src/main/java/com/epam/rd/autotasks/snail/Snail.java
@@ -1,9 +1,34 @@
 package com.epam.rd.autotasks.snail;
 
+import java.util.Scanner;
+
 public class Snail
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int h = scan.nextInt();
+        int days = 1;
+        int distance = a-b;
+        if (b>=a && a<h){
+            System.out.print("Impossible");
+            return;
+        }
+        else if (a>=h) {
+            System.out.println(days);
+            return;
+        }
+        while (distance<h){
+            days++;
+            distance = distance + a;
+            if (distance>=h) {
+                System.out.println(days);
+                return;
+            }
+            distance = distance - b;
+        }
         //Write a program that reads a,b and h (line by lyne in this order) and prints
         //the number of days for which the snail reach the top of the tree.
         //a - feet that snail travels up each day, b - feet that slides down each night, h - height of the tree
